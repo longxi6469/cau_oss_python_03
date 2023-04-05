795b4d14d6272a0e300548631b89fa9dc7d599d4
longxi6469 <Yonghee Lee>
2023-04-05 20:21:08 +0900

    ## Homework #2 구현

--- simple_calculator.py
+++ simple_calculator.py
@@ -16,6 +16,8 @@
 # 연산은 arithmetic_ops 함수를 호출하며 수행한다.
 # arithmetic_ops는 연산에 관한 함수로, 내부에서 사용자에게 정수 2개를 입력받은 뒤
 # 전달받은 매개함수로 정수를 전달한다
+def add(x,y): return x+y
+def sub(x,y): return x-y
 def arithmetic_ops(op):
     num1 = int(input("input 1st number:"))
     num2 = int(input("input 2nd number:"))
@@ -28,17 +30,23 @@ def arithmetic_ops(op):
 while True:
     op = input("input operation:")
     if op == "end":
-        [fill this blank]   # "반복을 종료"
+        break   # "반복을 종료"
     elif op == "+":
-        [fill this blank] = arithmetic_ops([fill this blank]) # 정의된 함수 사용
+        num1, num2, ret = arithmetic_ops(add) # 정의된 함수 사용
+    elif op == "-":
+        num1, num2, ret = arithmetic_ops(sub)    
     elif op == "*":
-        [fill this blank] = arithmetic_ops([fill this blank]) # 익명함수(lambda) 사용
+        num1, num2, ret = arithmetic_ops(lambda x, y : x*y) # 익명함수(lambda) 사용
+    elif op == "/":
+        num1, num2, ret = arithmetic_ops(lambda x, y : x/y)
+    elif op == "%":
+        num1, num2, ret = arithmetic_ops(lambda x, y : x%y)
     #
     # [fill this area] 위의 코드를 참고하여 -, /, %에 대한 내용 구현
     #
     else:
         print("Invalid operation")
-        [fill this blank] # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
+        continue# Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
     print(f"{num1}{op}{num2} = {ret}")  # 연산 결과를 출력
 
 print("Exit program")
